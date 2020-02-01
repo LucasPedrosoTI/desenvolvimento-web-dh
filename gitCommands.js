a4d3d6415d540afb2846c26a6a6175cdfbdac317
LucasPedrosoTI <lucas.pedrosoTI@gamil.com>
2020-02-01 14:04:26 -0300

    novos comandos do GIT, clone e pull

--- gitCommands.js
+++ gitCommands.js
@@ -7,3 +7,5 @@
 // git add . --> add all new files to commit
 // git commit -m "mensagem"
 // git push -u origin master
+// git clone <URL-do-projeto> --> clona os arquivos d projeto quando o PC não tem os arquivos
+// git pull origin master -> puxa os arquivos para a pasta existente da nuvem
