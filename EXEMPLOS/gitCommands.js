537c73280a88e3c9385a9922bc5484f7a5f9b589
LucasPedrosoTI <lucas.pedrosoTI@gamil.com>
2020-02-05 17:23:07 -0300

    aula de GIT 05-02-20

--- EXEMPLOS/gitCommands.js
+++ EXEMPLOS/gitCommands.js
@@ -9,3 +9,11 @@
 // git push -u origin master
 // git clone <URL-do-projeto> --> clona os arquivos d projeto quando o PC não tem os arquivos
 // git pull origin master -> puxa os arquivos para a pasta existente da nuvem
+//git log // todos os commits
+    // --oneline --> mostra apenas a mensagem e o codigo
+    // --graph --> mostra um grafico das alteracoes
+    // --all --> mostra todos os commits
+// git log --oneline // exibe log com hash e título do commit
+// git checkout "commit"  -> vai para o commit escolhido
+// git checkout - f --> descarta as alteraçoes feita em um commit anterior]
+// git branch "nome-da-branch" --> cria uma branch
---
+++ EXEMPLOS/terminalCommands.js
@@ -0,0 +1,13 @@
+// ls - lista arquivos 
+    // -lha -> lista com detalhes
+    // -R -> pasta, subpastas e arquivos 
+// cd - entra na pasta
+// pwd - mostra a pasta atual
+// mkdir - cria uma pasta
+// touch - cria um arquivo
+// echo - imprime uma msg no terminal
+// > - joga um comando para um arquivo
+// cat - imprime o conteudo de um arquivo
+// mv nome_do_arquivo nova_pasta - mover um arquivo e pode-se também renomear  
+// rm - remove um arquivo
+// rm -r -> remove uma pasta OU rmdir
