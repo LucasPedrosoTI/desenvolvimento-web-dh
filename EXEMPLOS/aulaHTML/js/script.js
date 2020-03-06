5b37829f0718d00ff332a4df2f59220202d3b425
lucaspedrosoti <lps1010@gmail.com>
2020-03-05 23:47:46 -0300

    included some responsive design, z-index, and positioning

---
+++ EXEMPLOS/aulaHTML/js/script.js
@@ -0,0 +1,15 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function closeMenu() {
+    document.getElementById("navbarSupportedContent").style.display = "none";
+}
+
+function openMenu() {
+    document.getElementById("navbarSupportedContent").style.display = "flex";
+}
