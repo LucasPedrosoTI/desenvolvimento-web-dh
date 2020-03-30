3d8bd230c3e4e31621e2b2d6b9b43b4027c48a6e
lucaspedrosoti <lps1010@gmail.com>
2020-03-30 17:29:14 -0300

    funcionalidade para página exclusiva da pizza e busca na página

--- EXEMPLOS/pizzariaExpressII/controllers/pizzasController.js
+++ EXEMPLOS/pizzariaExpressII/controllers/pizzasController.js
@@ -1,22 +1,51 @@
-const pizzas = require('../database/pizzas.json');
+const pizzas = require('../database/Pizzas.json');
 
 module.exports = {
     index: (req, res) => {
-        res.render('index', { pizzas }) // o mesmo que {pizza:pizza}
+        res.render("index", { pizzas });
+    },
+    show: (req, res) => {
+
+        // Capturando a pizza com o id passado na rota
+        let pizza = pizzas.find(
+            pizza => pizza.id == req.params.id
+        );
+
+        // Capturando a posição da pizza no array
+        let pos = pizzas.indexOf(pizza);
+
+        // determinando o id da próxima pizza e da anterior
+        let idPrev = null;
+        let idNext = null;
+
+        if (pos > 0) {
+            idPrev = pizzas[pos - 1].id;
+        }
+
+        if (pos < pizzas.length - 1) {
+            idNext = pizzas[pos + 1].id;
+        }
+
+        // Retornando a pizza para o usuário
+        if (pizza) {
+            res.render("pizza", { pizza, idNext, idPrev });
+        } else {
+            res.render("erros/pizzaNaoEncontrada", { id: req.params.id });
+        }
+    },
+    search: (req, res) => {
+        //capturar busca do usuário
+        let busca = req.query.busca;
+
+        // encotnrar as pizzas buscadas a partir dessa info
+        let pizzasEncontradas = pizzas.filter(pizza => pizza.nome.toLowerCase().includes(busca.toLowerCase()))
+
+        res.render('index', { pizzas: pizzasEncontradas })
     },
 
-    pizza: (req, res) => {
+    add: (req, res) => {
+        let { id } = req.body
 
-        //capturando a pizza com o id passado na rota
-        let pizza = pizzas.find(pizza => pizza.id == req.params.id);
-
-        // retornando a pizza ao usuário
-
-        if (pizza) {
-            res.render('pizza', { pizza });
-        } else {
-            res.render("error", { id: req.params.id })
-        }
+        res.send(id)
     }
-
 }
---
+++ EXEMPLOS/pizzariaExpressII/routes/PizzasRouter.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+const PizzasController = require('../controllers/PizzasController');
+
+/* GET home page. */
+router.get('/', PizzasController.index);
+router.get('/pizzas/busca', PizzasController.search)
+router.get('/pizzas/:id', PizzasController.show);
+router.post('/pedidos/add', PizzasController.add)
+
+module.exports = router;
